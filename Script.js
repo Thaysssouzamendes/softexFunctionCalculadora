3c007a041b4410aa556a231508fec3a297d92ceb
Thays Mendes <123996983+Thaysssouzamendes@users.noreply.github.com>
2023-11-19 11:51:03 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 // Função tradicional sem parâmetros
 function saudacao() {
-    console.log("Olá! Bem-vindo à calculadora.");
+    console.log("Bem-vindo aoPrograma calculadora.");
 }
 
 // Função tradicional com parâmetros e retorno de valor
@@ -11,11 +11,11 @@ function somar(a, b) {
 // Arrow function com parâmetros e retorno de valor
 const multiplicar = (a, b) => a * b;
 
-// Utilizando as funções
+// Utilizando as três funções
 saudacao();
 
-let numero1 = 10;
-let numero2 = 5;
+let numero1 = 25;
+let numero2 = 10;
 
 let resultadoSoma = somar(numero1, numero2);
 console.log(`A soma de ${numero1} e ${numero2} é: ${resultadoSoma}`);
